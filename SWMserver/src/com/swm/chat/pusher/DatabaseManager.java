cc2a2d694c06776c268b8de1be2e48eff29ecbde
Kim Junhyoung <ebsud89@gmail.com>
2014-08-10 15:45:10 +0900

    tag

--- SWMserver/src/com/swm/chat/pusher/DatabaseManager.java
+++ SWMserver/src/com/swm/chat/pusher/DatabaseManager.java
@@ -18,7 +18,11 @@ public class DatabaseManager {
 	// 상황 별 푸시 종류 
 	
 	// 1. msg 왔을 때의 푸시
-	// 2. deal 상태가 변했을 때의 푸시
+	// 2. 입주 신청이 있을때 
+	// 3. 질문글이 올라왔을때
+	// 4. 답변이 달렸을때
+	// 5. 입력했던 정보에 대해서 새로운 방이 떳을때
+	// 6. 
 	
 
 //	public ArrayList<E>> getAllmsg () {
