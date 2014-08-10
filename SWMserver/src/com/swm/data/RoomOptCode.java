31f6893cc6624bed73d8676bd281482ecc71bec0
aaqbtm <aaqbtm@naver.com>
2014-08-10 15:51:09 +0900

    aaqbtm data패키지에 RoomInfoCode와 RoomOptCode

--- SWMserver/src/com/swm/data/RoomOptCode.java
+++ SWMserver/src/com/swm/data/RoomOptCode.java
@@ -1,5 +1,24 @@
 package com.swm.data;
 
-public class RoomOptCode {
+import java.util.HashMap;
 
+public class RoomOptCode {
+	private HashMap<Integer, String> roomopt = new HashMap<Integer, String>();
+
+	public RoomOptCode() {
+		this.roomopt.put(1, "wifi");
+		this.roomopt.put(2, "bed");
+		this.roomopt.put(3, "desk");
+		this.roomopt.put(4, "wardrobe");
+		this.roomopt.put(5, "sofa");
+		this.roomopt.put(6, "airconditioner");
+		this.roomopt.put(7, "refrigerator");
+		this.roomopt.put(8, "washingmachine");
+		this.roomopt.put(9, "tv");
+		this.roomopt.put(10, "gas stove");
+		this.roomopt.put(11, "microwave");
+		this.roomopt.put(12, "oven");
+		this.roomopt.put(13, "bathtub");
+
+	}
 }
